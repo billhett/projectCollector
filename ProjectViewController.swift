f9695c1ae9e300d055b4b83f24d507cded4ea4df
William Hettinger <bill.hettinger@gmail.com>
2018-02-27 16:20:09 -0500

    set up storyboard and projectViewController file

---
+++ ProjectViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ProjectViewController.swift
+//  projectCollection
+//
+//  Created by William Hettinger on 2/27/18.
+//  Copyright © 2018 William Hettinger. All rights reserved.
+//
+
+import UIKit
+
+class ProjectViewController: UIViewController {
+
+    @IBOutlet weak var projectImageView: UIImageView!
+    @IBOutlet weak var titleTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+
+    @IBAction func photosTapped(_ sender: Any) {
+    }
+    @IBAction func cameraTapped(_ sender: Any) {
+    }
+    
+    @IBAction func addTapped(_ sender: Any) {
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+
+}
